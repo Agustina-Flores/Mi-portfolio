8199e406b1232997cd7ed087cba64f693fa0517d
Agustina <agustina.flores@wirsolut.com>
2025-07-26 22:36:28 -0300

    arreglo diseño

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
 }, []);
 
   return (
-    <div className="App">
+    <div className="App overflow-x-hidden">
       <Title />
       <Header />
       <AboutMe/>
--- src/componentes/Title.jsx
+++ src/componentes/Title.jsx
@@ -6,12 +6,13 @@ export const Title = () =>
 return (
 <div
   id="inicio"
-  className="bg-gradient-to-r from-blue-500 via-indigo-700 to-purple-600 text-white min-h-screen flex flex-col items-center justify-center px-4 md:px-6 overflow-x-hidden"
->
+  className="bg-gradient-to-r from-blue-500 via-indigo-700 to-purple-600 text-white min-h-screen flex flex-col items-center justify-center px-6 sm:px-8 md:px-12 overflow-x-hidden"
+
+> 
   <AnimatedTitle
-    text="Hola, soy Agustina Flores! 👩‍💻"
-    className="text-3xl sm:text-4xl md:text-5xl font-bold mb-4 text-center"
-  />
+    text="Hola 👩‍💻, soy Agustina Flores!"
+    className="break-words text-center text-white text-2xl font-bold"
+  /> 
   <AnimatedTitle
     className="text-base sm:text-lg md:text-xl max-w-xl text-center leading-relaxed font-sans"
     text="Desarrolladora Fullstack Junior"
